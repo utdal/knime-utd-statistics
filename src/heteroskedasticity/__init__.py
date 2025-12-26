6b674c78603676cc5579c43b7e15e9233bdaf35a
Ahmed Elghazi <aelghazi6@gmail.com>
2025-12-26 15:23:27 -0600

    Basic vs Advanced working, new formatting to remove scientific notation formatting, confirmed running in KNIME without errors. Next step is to validate output, and possibly change the config depending on test selected since some tests have different hyperparameters

--- src/heteroskedasticity/__init__.py
+++ src/heteroskedasticity/__init__.py
@@ -22,11 +22,7 @@ Exported Classes and Parameters:
 - alpha_param, gq_sort_variable_param, gq_split_fraction_param: Additional parameters
 """
 
-from .heteroskedasticity_tests import (
-    run_breusch_pagan_test,
-    run_white_test,
-    run_goldfeld_quandt_test
-)
+from .heteroskedasticity_tests import run_breusch_pagan_test, run_white_test, run_goldfeld_quandt_test
 
 from .utils import (
     TestType,
@@ -37,38 +33,32 @@ from .utils import (
     gq_sort_variable_param,
     gq_split_fraction_param,
     is_numeric,
-    detect_categorical_columns
+    detect_categorical_columns,
+    format_p_value,
 )
 
-from .regression_core import (
-    prepare_data,
-    fit_ols_model,
-    generate_predictions_residuals,
-    extract_model_summary
-)
+from .regression_core import prepare_data, fit_ols_model, generate_predictions_residuals, extract_model_summary
 
 __all__ = [
     # Test functions
-    'run_breusch_pagan_test',
-    'run_white_test',
-    'run_goldfeld_quandt_test',
-    
+    "run_breusch_pagan_test",
+    "run_white_test",
+    "run_goldfeld_quandt_test",
     # Parameters
-    'TestType',
-    'test_type_param',
-    'target_column_param',
-    'predictor_columns_param',
-    'alpha_param',
-    'gq_sort_variable_param',
-    'gq_split_fraction_param',
-    
+    "TestType",
+    "test_type_param",
+    "target_column_param",
+    "predictor_columns_param",
+    "alpha_param",
+    "gq_sort_variable_param",
+    "gq_split_fraction_param",
     # Utilities
-    'is_numeric',
-    'detect_categorical_columns',
-    
+    "is_numeric",
+    "detect_categorical_columns",
+    "format_p_value",
     # Regression functions
-    'prepare_data',
-    'fit_ols_model',
-    'generate_predictions_residuals',
-    'extract_model_summary'
+    "prepare_data",
+    "fit_ols_model",
+    "generate_predictions_residuals",
+    "extract_model_summary",
 ]
