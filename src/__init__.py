1416c67b6892319a39aa4cc9a5682a71a1601718
Ahmed Elghazi <aelghazi6@gmail.com>
2026-02-28 18:04:10 -0600

    First commit, output table seems very simple at first glance but MANOVA designed to be the gatekeeper before the ANOVA

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,6 @@ from .normality_node import NormalityTestsNode
 from .post_hoc_node import PostHocTestsNode
 from .heteroskedasticity_node import HeteroskedasticityNode
 from .factorial_anova_node import FactorialAnovaNode
+from .manova_node import ManovaNode
 
-__all__ = ["NormalityTestsNode", "PostHocTestsNode", "FactorialAnovaNode", "HeteroskedasticityNode"]
+__all__ = ["NormalityTestsNode", "PostHocTestsNode", "HeteroskedasticityNode", "ManovaNode", "FactorialAnovaNode"]
