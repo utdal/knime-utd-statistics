0d59196cba3badc761b7ff98b8494cd29f403682
Ahmed Elghazi <aelghazi6@gmail.com>
2026-03-24 19:51:57 -0500

    starter code, failing in KNIME. Work in progress.
    # Conflicts:
    #	src/__init__.py
    
    # Conflicts:
    #	src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .normality_node import NormalityTestsNode
 from .post_hoc_node import PostHocTestsNode
 from .heteroskedasticity_node import HeteroskedasticityNode
+from .factorial_anova_node import FactorialAnovaNode
 
-__all__ = ["NormalityTestsNode", "PostHocTestsNode", "NormalityTestsNode", "HeteroskedasticityNode"]
+__all__ = ["NormalityTestsNode", "PostHocTestsNode", "FactorialAnovaNode", "HeteroskedasticityNode"]
